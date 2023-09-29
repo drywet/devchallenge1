bcc7b8970d1d8362966d6d7573a33cc86edefd4c
Marat Sahabudinov <m.sahabudinov@gmail.com>
2023-09-29 23:58:17 +0300

    Add a todo

--- src/main/scala/pkg/Service.scala
+++ src/main/scala/pkg/Service.scala
@@ -3,5 +3,6 @@ package pkg
 import java.util.concurrent.ConcurrentHashMap
 
 class Service {
+  // TODO sheet_id is case-insensitive
   val sheets: ConcurrentHashMap[String, Sheet] = new ConcurrentHashMap()
 }
